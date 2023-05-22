d65b5a5df8aba02b7ba57f0e7e44753cbcc200d5
dei <allamegod@gmail.com>
2023-05-23 01:02:12 +0300

    undo

--- main.py
+++ main.py
@@ -4,54 +4,10 @@ import datetime
 
 from sys import platform
 if platform == "win32":
-	from ansicon import load
-	load()
+    from ansicon import load
+    load()
 
 bot = commands.Bot(command_prefix="$", help_command=None, intents=discord.Intents.all())
-
-def dayDeclension(number):
-    listOfNumbers = []
-    tempString = str(number)
-    for symbol in tempString:
-        listOfNumbers.append(symbol)
-    listOfNumbers.reverse()
-    if len(listOfNumbers) == 2:
-        match listOfNumbers[0]:
-            case '1':
-                if listOfNumbers[1] == '1':
-                    return "дней"
-                else:
-                    return "день"
-            case '2':
-                if listOfNumbers[1] != '1':
-                    return "дня"
-                else:
-                    return "дней"
-            case '3':
-                if listOfNumbers[1] != '1':
-                    return "дня"
-                else:
-                    return "дней"
-            case '4':
-                if listOfNumbers[1] != '1':
-                    return "дня"
-                else:
-                    return "дней"
-            case _:
-                return "дней"
-            
-    else:
-        match listOfNumbers[0]:
-            case '1':
-                return "день"
-            case '2':
-                return "дня"
-            case '3':
-                return "дня"
-            case '4':
-                return "дня"
-            case _:
-                return "дней"
 
 @bot.event
 async def on_ready():
@@ -67,6 +23,7 @@ async def on_ready():
 
 @tasks.loop(minutes=1)
 async def checkTime():
+    print("checkTime")
     #dates
     geoDaysLeft = datetime.date(2023, 5, 26) - datetime.date.today()
     rusDaysLeft = datetime.date(2023, 5, 29) - datetime.date.today()
@@ -78,10 +35,6 @@ async def checkTime():
     engtDaysLeft = datetime.date(2023, 6, 27) - datetime.date.today()
     bgeoDaysLeft = datetime.date(2023, 7, 11) - datetime.date.today()
     #dates_end
-
-    # склонения
-    
-    #
 
     channel = bot.get_channel(1109578326005256333)
     with open("date.txt", 'r+') as date:
@@ -95,33 +48,17 @@ async def checkTime():
             delta = now-lastTime
             if delta.days >= 1:
                 embed = discord.Embed(title="GENTLEMEN, SYNCHRONIZE YOUR DEATH WATCHES:")
-                embed.add_field(name=f"{(lambda: '• ГЕОГРАФИИ:', lambda: '~~• ГЕОГРАФИИ:~~')[geoDaysLeft.days <= 0]()}", value=f"{(lambda:'**{geoDaysLeft.days}** {dayDeclension(geoDaysLeft.days)} осталось', lambda:'~~**0** дней осталось~~')[geoDaysLeft.days <= 0]()}", inline=False)
-                embed.add_field(name=f"{(lambda: '• РУССКИЙ:', lambda: '~~• РУССКИЙ:~~')[rusDaysLeft.days <= 0]()}", value=f"{(lambda:'**{rusDaysLeft.days}** {dayDeclension(rusDaysLeft.days)} осталось', lambda:'~~**0** дней осталось~~')[rusDaysLeft.days <= 0]()}", inline=False)
-                embed.add_field(name=f"{(lambda: '• МАТЕМАТИКА:', lambda: '~~• МАТЕМАТИКА:~~')[matDaysLeft.days <= 0]()}", value=f"{(lambda:'**{matDaysLeft.days}** {dayDeclension(matDaysLeft.days} осталось', lambda:'~~**0** дней осталось~~')[matDaysLeft.days <= 0]()}", inline=False)
-                embed.add_field(name=f"{(lambda: '• АНГЛИЙСКИЙ:', lambda: '~~• АНГЛИЙСКИЙ:~~')[engDaysLeft.days <= 0]()}", value=f"{(lambda:'**{engDaysLeft.days}** {dayDeclension(engDaysLeft.days)} осталось', lambda:'~~**0** дней осталось~~')[engDaysLeft.days <= 0]()}", inline=False)
-                embed.add_field(name=f"{(lambda: '• РУССКИЙ (Беларусь):', lambda: '~~• РУССКИЙ (Беларусь):~~')[brusDaysLeft.days <= 0]()}", value=f"{(lambda:'**{brusDaysLeft.days}** {dayDeclension(brusDaysLeft.days})} осталось', lambda:'~~**0** дней осталось~~')[brusDaysLeft.days <= 0]()}", inline=False)
-                embed.add_field(name=f"{(lambda: '• ИНФОРМАТИКА:', lambda: '~~• ИНФОРМАТИКА:~~')[infDaysLeft.days <= 0]()}", value=f"{(lambda:'**{infDaysLeft.days}** {dayDeclension(infDaysLeft.days)} осталось', lambda:'~~**0** дней осталось~~')[infDaysLeft.days <= 0]()}", inline=False)
-                embed.add_field(name=f"{(lambda: '• МАТЕМАТИКА (Беларусь):', lambda: '~~• МАТЕМАТИКА (Беларусь):~~')[bmatDaysLeft.days <= 0]()}", value=f"{(lambda:'**{bmatDaysLeft.days}** {dayDeclension(bmatDaysLeft.days)} осталось', lambda:'~~**0** дней осталось~~')[bmatDaysLeft.days <= 0]()}", inline=False)
-                embed.add_field(name=f"{(lambda: '• АНГЛИЙСКИЙ (ГОВОРЕНИЕ):', lambda: '~~• АНГЛИЙСКИЙ (ГОВОРЕНИЕ):~~')[engtDaysLeft.days <= 0]()}", value=f"{(lambda:'**{engtDaysLeft.days}** {dayDeclension(engtDaysLeft.days)} осталось', lambda:'~~**0** дней осталось~~')[engtDaysLeft.days <= 0]()}", inline=False)
-                embed.add_field(name=f"{(lambda: '• ГЕОГРАФИЯ (Беларусь):', lambda: '~~• ГЕОГРАФИЯ (Беларусь):~~')[bgeoDaysLeft.days <= 0]()}", value=f"**{(lambda:'**{bgeoDaysLeft.days}** {dayDeclension(bgeoDaysLeft.days)} осталось', lambda:'~~**0** дней осталось~~')[bgeoDaysLeft.days <= 0]()}", inline=False)
-                if geoDaysLeft.days == 0:
-                    await channel.send("@everyone https://www.youtube.com/watch?v=pjGZnRwtvww", embed=embed)
-                elif rusDaysLeft.days == 0:
-                    await channel.send("@everyone https://www.youtube.com/watch?v=TunxTKRvIk8", embed=embed)
-                elif matDaysLeft.days == 0:
-                    await channel.send("@everyone https://tenor.com/view/crying-tears-terrified-scared-gif-25745077", embed=embed)
-                elif engDaysLeft.days == 0:
-                    await channel.send("@everyone https://www.youtube.com/watch?v=yF3JWJksP9I", embed=embed)
-                elif brusDaysLeft.days == 0:
-                    await channel.send("@everyone https://www.youtube.com/watch?v=_GQ_sKMZ-mE", embed=embed)
-                elif infDaysLeft.days == 0:
-                    await channel.send("@everyone https://www.youtube.com/watch?v=PdA7JUcVdL8", embed=embed)
-                elif bmatDaysLeft.days == 0:
-                    await channel.send("@everyone https://www.youtube.com/watch?v=gqKDC5_EUIk", embed=embed)
-                elif engtDaysLeft.days == 0:
-                    await channel.send("@everyone https://www.youtube.com/watch?v=GSyCkS36R0E", embed=embed)
-                elif bgeoDaysLeft.days == 0:
-                    await channel.send("@everyone https://www.youtube.com/watch?v=yfPfH26IdKk", embed=embed)
+                embed.add_field(name="• ГЕОГРАФИИ:", value=f"**{geoDaysLeft.days}** дней осталось", inline=False)
+                embed.add_field(name="• РУССКИЙ:", value=f"**{rusDaysLeft.days}** дней осталось", inline=False)
+                embed.add_field(name="• МАТЕМАТИКА:", value=f"**{matDaysLeft.days}** дней осталось", inline=False)
+                embed.add_field(name="• АНГЛИЙСКИЙ:", value=f"**{engDaysLeft.days}** дней осталось", inline=False)
+                embed.add_field(name="• РУССКИЙ (Беларусь):", value=f"**{brusDaysLeft.days}** дней осталось", inline=False)
+                embed.add_field(name="• ИНФОРМАТИКА:", value=f"**{infDaysLeft.days}** дней осталось", inline=False)
+                embed.add_field(name="• МАТЕМАТИКА (Беларусь):", value=f"**{bmatDaysLeft.days}** дней осталось", inline=False)
+                embed.add_field(name="• АНГЛИЙСКИЙ (ГОВОРЕНИЕ):", value=f"**{engtDaysLeft.days}** дней осталось", inline=False)
+                embed.add_field(name="• ГЕОГРАФИЯ (Беларусь):", value=f"**{bgeoDaysLeft.days}** дней осталось", inline=False)
+            
+                await channel.send("@everyone", embed=embed)
                 
                 date.seek(0)
                 date.truncate()
@@ -129,7 +66,6 @@ async def checkTime():
                 date.write(now)
             else:
                 pass
-                
 
             
 from json import load
